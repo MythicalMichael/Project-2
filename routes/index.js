5ebaed02c102078e919e829da2b8378fd1025089
Sarah Fontes <sarahfontes@Sarahs-MacBook-Pro.local>
2017-11-22 16:10:28 +0100

    doing: tasks-members

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('dashboard');
+
+router.get('/', (req, res, next) => {
+  Group.findOne({_id: groupId}, (err, group) => {
+      if (err) return next(err);
+      res.render('dashboard', group);
+  });
 });
-
-
 
 module.exports = router;
