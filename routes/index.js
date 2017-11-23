ad336d8f0200c51cd799971c98a94a6597e11bc6
Michal Cizevskij <michael.chizhevskiy@hotmail.com>
2017-11-23 17:25:40 +0100

    redirect to welcome works again

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const Group = require("../models/group");
 const Task = require("../models/task");
 const User = require("../models/user");
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+
+
 /* GET home page. */
 
-router.get('/', (req, res, next) => {
+router.get('/', ensureLogin.ensureLoggedIn("/welcome"),(req, res, next) => {
 
   const userId = req.user._id;
   const user = req.user;
